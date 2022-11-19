73ff60576ac8d93a651afaf2c2679da3c96561d3
ehtesham2012 <104604060+ehtesham2012@users.noreply.github.com>
2022-11-20 02:08:38 +0530

    Update file_read_delete.sh

--- file_read_delete.sh
+++ file_read_delete.sh
@@ -5,8 +5,11 @@ mkdir /filestore
 chmod -R 777 /incoming
 chmod -R /filestore
 
+while
 for f in /incoming/*.txt; 
 do 
 	head -c -20 $f > "/filestore/new_$f";
 	rm -rf $f
 done
+sleep 1m
+do :; done
