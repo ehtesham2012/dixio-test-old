aad9c3a580a8c3643ef42b747ae28b9386c89163
ehtesham2012 <104604060+ehtesham2012@users.noreply.github.com>
2022-11-20 02:05:27 +0530

    Update file_read_delete.sh

--- file_read_delete.sh
+++ file_read_delete.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-for f in *.txt; 
+sudo su -
+mkdir /incoming
+mkdir /filestore
+chmod -R 777 /incoming
+chmod -R /filestore
+
+for f in /incoming/*.txt; 
 do 
-	head -c -20 $f > "new_$f";
+	head -c -20 $f > "/filestore/new_$f";
 	rm -rf $f
 done
