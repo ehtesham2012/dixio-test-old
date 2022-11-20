49cec1a279c3570dbeaba6129f7f5478c0943702
ehtesham2012 <104604060+ehtesham2012@users.noreply.github.com>
2022-11-20 12:49:21 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
 sudo git clone https://github.com/ehtesham2012/dixio-test.git
-sudo ./dixio-test/file_read_delete.sh >> "file_read_delete.log"
+sudo nohup ksh /dixio-test/file_read_delete.sh >> "file_read_delete.log"
