8edce273b7943c262672a35943bf8abffeb3e050
ehtesham2012 <104604060+ehtesham2012@users.noreply.github.com>
2022-11-20 13:57:26 +0530

    Update file_read_delete.sh

--- file_read_delete.sh
+++ file_read_delete.sh
@@ -1,16 +1,20 @@
-#!/bin/sh -x
-sudo su -
+#!/bin/sh
 mkdir /incoming
 mkdir /filestore
 chmod -R 777 /incoming
 chmod -R 777 /filestore
 
 cd /incoming
-while
+while :
+echo "loop started"
 for f in *.txt; 
 do 
 	head -c -20 $f > "/filestore/new_$f";
 	rm -rf /incoming/$f
 done
-sleep 1m
-do :; done
+echo "sleep started"
+sleep 60
+echo "sleep ended"
+do 
+echo "runs every 5 mins"
+done
